2786bcb4c9d3a10093e83132cb06d613a2b3afc0
username <useremail>
2023-06-01 12:50:05 -0700

    Intro, questions, Story start

--- CIS1110_Course_Project.py
+++ CIS1110_Course_Project.py
@@ -1,1 +1,23 @@
+#Introduction
+print(f"\nWhy hello there, and welcome to a wonderous story about a peppy little bard who is traveling through the forest trying to get home.  ")
+print(f"\nBut before we begin I need your help in telling the story by answering a few questions for me.  ")
+print(f"\nAfter typing your answer please make sure to press enter to move onto the next question.  ")
+input(f"\nPress the enter key to continue...  ")
+
+#5 Questions for inputs used in story
+bardName = input("What is the bards name:  ")
+instrument = input("What instrument does the bard play:  ")
+numberOfBirds = input("What is your favorite number:  ")
+forestName = input("Choose a noun:  ")
+pondName = input("\nChoose a second noun:  ")
+print("\nThank you for your help! Now let the story begin!  ")
+
+#Story Begins
+print("Long ago there was a bard named (bardName) who lived in a forest called (forestName).  ")
+print("(bardName) loved to take walks through the (forestName)) forest and play their (instrument) for any woodland creatures they would run into.  ")
+print("(bardName) even loved to sing to dance and sing to the woodland creatures sometimes.  ")
+print("Oneday (bardName) was walking through the forest when he heard the most beautiful sound.  ")
+
+#Decision 1
+
 
